55bf619268a8839936fa08717a5b395bc49508e0
dabear <bjorninge@gmail.com>
2019-04-21 02:15:03 +0200

    tableview can now support more schedules in the future

--- MiaomiaoClientUI/AlarmSettingsTableViewController.swift
+++ MiaomiaoClientUI/AlarmSettingsTableViewController.swift
@@ -76,24 +76,24 @@ public class AlarmSettingsTableViewController: UITableViewController, AlarmTimeI
     public var maximumBasalRatePerHour: Double? {
         didSet {
             /*if isViewLoaded, let cell = tableView.cellForRow(at: IndexPath(row: 0, section: Section.basalRate.rawValue)) as? TextFieldTableViewCell {
-                if let maximumBasalRatePerHour = maximumBasalRatePerHour {
-                    cell.textField.text = valueNumberFormatter.string(from:  maximumBasalRatePerHour)
-                } else {
-                    cell.textField.text = nil
-                }
-            }*/
+             if let maximumBasalRatePerHour = maximumBasalRatePerHour {
+             cell.textField.text = valueNumberFormatter.string(from:  maximumBasalRatePerHour)
+             } else {
+             cell.textField.text = nil
+             }
+             }*/
         }
     }
     
     public var maximumBolus: Double? {
         didSet {
             /*if isViewLoaded, let cell = tableView.cellForRow(at: IndexPath(row: 0, section: Section.bolus.rawValue)) as? TextFieldTableViewCell {
-                if let maximumBolus = maximumBolus {
-                    cell.textField.text = valueNumberFormatter.string(from: maximumBolus)
-                } else {
-                    cell.textField.text = nil
-                }
-            }*/
+             if let maximumBolus = maximumBolus {
+             cell.textField.text = valueNumberFormatter.string(from: maximumBolus)
+             } else {
+             cell.textField.text = nil
+             }
+             }*/
         }
     }
     
@@ -104,7 +104,7 @@ public class AlarmSettingsTableViewController: UITableViewController, AlarmTimeI
         for var cell in tableView.visibleCells {
             let index = tableView.indexPath(for: cell)
             //todo: replace with dynamic check of number of schedules
-            if let section = index?.section, section >  Section.scheduleCount{
+            if let section = index?.section, section >  glucoseSchedulesCount{
                 
                 break
             }
@@ -136,7 +136,7 @@ public class AlarmSettingsTableViewController: UITableViewController, AlarmTimeI
     
     private var isSyncInProgress = false {
         didSet {
-           
+            
             for cell in tableView.visibleCells {
                 switch cell {
                 case let cell as TextButtonTableViewCell:
@@ -156,6 +156,7 @@ public class AlarmSettingsTableViewController: UITableViewController, AlarmTimeI
             navigationItem.hidesBackButton = isSyncInProgress
         }
     }
+    private lazy var glucoseSchedules : GlucoseScheduleList? = UserDefaults.standard.glucoseSchedules
     
     private lazy var valueNumberFormatter: NumberFormatter = {
         let formatter = NumberFormatter()
@@ -170,9 +171,8 @@ public class AlarmSettingsTableViewController: UITableViewController, AlarmTimeI
     public init(glucoseUnit: HKUnit) {
         self.glucoseUnit = glucoseUnit
         
-        
-        print("loaded glucose schedule was \(UserDefaults.standard.glucoseSchedules)")
         super.init(style: .grouped)
+        print("loaded glucose schedule was \(glucoseSchedules)")
         
     }
     
@@ -192,17 +192,14 @@ public class AlarmSettingsTableViewController: UITableViewController, AlarmTimeI
     
     // MARK: - Table view data source
     
-    private enum Section: Int {
-        case schedule1
-        case schedule2
-        
-        case sync
-        
-        static let count=3
-        static let scheduleCount = 2
+    
+    private let minimumSchedulesCount = 2
+   
+    
+    private var glucoseSchedulesCount : Int{
+        let count = glucoseSchedules?.schedules?.count ?? 0
+        return count >= minimumSchedulesCount ? count : minimumSchedulesCount
     }
-    
-    
     
     private enum ScheduleRow : Int {
         case timerange
@@ -214,39 +211,38 @@ public class AlarmSettingsTableViewController: UITableViewController, AlarmTimeI
     
     
     public override func numberOfSections(in tableView: UITableView) -> Int {
-        return Section.count
+        //dynamic number of schedules + sync row
+        return glucoseSchedulesCount + 1
         
     }
     
     public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        switch Section(rawValue: section)! {
-        case .schedule1:
+        /*switch Section(rawValue: section)! {
+         case .schedule1:
+         return ScheduleRow.count
+         case .schedule2:
+         return ScheduleRow.count
+         case .sync:
+         return 1
+         }*/
+        
+        switch section {
+        case let x where x < glucoseSchedulesCount:
             return ScheduleRow.count
-        case .schedule2:
-            return ScheduleRow.count
-        case .sync:
+        default:
             return 1
         }
+        
+        
     }
     
     public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        switch Section(rawValue: indexPath.section)! {
         
-        case .schedule1, .schedule2:
-            /*
-             switch LatestReadingRow(rawValue: indexPath.row)! {
-             case .glucose:
-             */
-           
-            /*let cell = tableView.dequeueReusableCell(withIdentifier: TextFieldTableViewCell.className, for: indexPath) as!  TextFieldTableViewCell
+        
+        switch (indexPath.section) {
             
+        case let x where x < glucoseSchedulesCount:
             
-           
-            cell.textField.keyboardType = .decimalPad
-            cell.textField.placeholder = isReadOnly ? LocalizedString("Enter a low glucose alarm value", comment: "The placeholder text instructing users how to enter a low glucose value") : nil
-            cell.textField.isEnabled = !isReadOnly && !isSyncInProgress
-            cell.unitLabel?.text = LocalizedString("mgdl", comment: "The unit string for low glucose")
-            */
             let bundle = Bundle(for: type(of: self))
             
             switch ScheduleRow(rawValue: indexPath.row)! {
@@ -259,6 +255,7 @@ public class AlarmSettingsTableViewController: UITableViewController, AlarmTimeI
                 cell.delegate = self
                 
                 
+                
                 cell.iconImageView.image = UIImage(named: "icons8-schedule-50", in: bundle, compatibleWith: traitCollection)
                 
                 return cell
@@ -267,12 +264,14 @@ public class AlarmSettingsTableViewController: UITableViewController, AlarmTimeI
                 cell.alarmType = GlucoseAlarmType.low
                 cell.minValueTextField.placeholder = "glucose"
                 cell.unitString = self.glucoseUnit.localizedShortUnitString
+                cell.minValueTextField.keyboardType = .decimalPad
                 cell.iconImageView.image = UIImage(named: "icons8-drop-down-arrow-50", in: bundle, compatibleWith: traitCollection)
                 return cell
             case .highglucose:
                 let cell = tableView.dequeueReusableCell(withIdentifier: GlucoseAlarmInputCell.className, for: indexPath) as!  GlucoseAlarmInputCell
                 
                 cell.alarmType = GlucoseAlarmType.high
+                cell.minValueTextField.keyboardType = .decimalPad
                 
                 cell.unitString = self.glucoseUnit.localizedShortUnitString
                 cell.minValueTextField.placeholder = "glucose"
@@ -281,7 +280,7 @@ public class AlarmSettingsTableViewController: UITableViewController, AlarmTimeI
                 return cell
             }
             
-        case .sync:
+        default: //case .sync:
             let cell = tableView.dequeueReusableCell(withIdentifier: TextButtonTableViewCell.className, for: indexPath) as! TextButtonTableViewCell
             
             cell.textLabel?.text = LocalizedString("Save glucose alarms", comment: "The title for Save glucose alarms")
@@ -293,25 +292,26 @@ public class AlarmSettingsTableViewController: UITableViewController, AlarmTimeI
     }
     
     public override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        switch Section(rawValue: section)! {
-        case .schedule1:
-            return LocalizedString("Glucose Alarm Schedule #1", comment: "The title text for the Glucose Alarm Schedule #1")
-        case .schedule2:
-            return LocalizedString("Glucose Alarm Schedule #2", comment: "The title text for the Glucose Alarm Schedule #2")
-        case .sync:
+        switch section {
+        case let x where x < glucoseSchedulesCount:
+            
+            return LocalizedString("Glucose Alarm Schedule", comment: "The title text for the Glucose Alarm Schedule") + " #\(section+1)"
+            
+        default: //case .sync:
             return nil
         }
     }
     
     public override func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
-        switch Section(rawValue: section)! {
-        case .schedule1:
-            return nil
-        case .schedule2:
-            return nil
-        case .sync:
-            return nil //LocalizedString("Save alarms", comment: "The title for saving alarms")
-        }
+        return nil
+        /*switch Section(rawValue: section)! {
+         case .schedule1:
+         return nil
+         case .schedule2:
+         return nil
+         case .sync:
+         return nil //LocalizedString("Save alarms", comment: "The title for saving alarms")
+         }*/
     }
     
     public override func tableView(_ tableView: UITableView, shouldHighlightRowAt indexPath: IndexPath) -> Bool {
@@ -319,8 +319,8 @@ public class AlarmSettingsTableViewController: UITableViewController, AlarmTimeI
     }
     
     public override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        switch Section(rawValue: indexPath.section)! {
-        case .schedule1, .schedule2:
+        switch indexPath.section {
+        case let x where x < glucoseSchedulesCount:
             if let cell = tableView.cellForRow(at: indexPath) as? TextFieldTableViewCell {
                 if cell.textField.isFirstResponder {
                     cell.textField.resignFirstResponder()
@@ -328,7 +328,7 @@ public class AlarmSettingsTableViewController: UITableViewController, AlarmTimeI
                     cell.textField.becomeFirstResponder()
                 }
             }
-        case .sync:
+        default: //case .sync:
             tableView.endEditing(true)
             
             isSyncInProgress = true
@@ -350,23 +350,23 @@ public class AlarmSettingsTableViewController: UITableViewController, AlarmTimeI
             }
             
             /*syncSource.syncDeliveryLimitSettings(for: self) { (result) in
-                DispatchQueue.main.async {
-                    switch result {
-                    case .success(maximumBasalRatePerHour: let maxBasal, maximumBolus: let maxBolus):
-                        self.maximumBasalRatePerHour = maxBasal
-                        self.maximumBolus = maxBolus
-                        
-                        self.delegate?.deliveryLimitSettingsTableViewControllerDidUpdateMaximumBasalRatePerHour(self)
-                        self.delegate?.deliveryLimitSettingsTableViewControllerDidUpdateMaximumBolus(self)
-                        
-                        self.isSyncInProgress = false
-                    case .failure(let error):
-                        self.presentAlertController(with: error, animated: true, completion: {
-                            self.isSyncInProgress = false
-                        })
-                    }
-                }
-            }*/
+             DispatchQueue.main.async {
+             switch result {
+             case .success(maximumBasalRatePerHour: let maxBasal, maximumBolus: let maxBolus):
+             self.maximumBasalRatePerHour = maxBasal
+             self.maximumBolus = maxBolus
+             
+             self.delegate?.deliveryLimitSettingsTableViewControllerDidUpdateMaximumBasalRatePerHour(self)
+             self.delegate?.deliveryLimitSettingsTableViewControllerDidUpdateMaximumBolus(self)
+             
+             self.isSyncInProgress = false
+             case .failure(let error):
+             self.presentAlertController(with: error, animated: true, completion: {
+             self.isSyncInProgress = false
+             })
+             }
+             }
+             }*/
         }
         
         tableView.deselectRow(at: indexPath, animated: true)
@@ -374,27 +374,3 @@ public class AlarmSettingsTableViewController: UITableViewController, AlarmTimeI
 }
 
 
-/*extension AlarmSettingsTableViewController: TextFieldTableViewCellDelegate {
-    public func textFieldTableViewCellDidBeginEditing(_ cell: TextFieldTableViewCell) {
-    }
-    
-    public func textFieldTableViewCellDidEndEditing(_ cell: TextFieldTableViewCell) {
-        guard let indexPath = tableView.indexPath(for: cell) else {
-            return
-        }
-        
-        let value = valueNumberFormatter.number(from: cell.textField.text ?? "")?.doubleValue
-        
-        switch Section(rawValue: indexPath.section)! {
-        case .highGlucoseAlarm:
-            //maximumBasalRatePerHour = value
-           break
-        case .lowGlucoseAlarm:
-            //maximumBolus = value
-            break
-        case .sync:
-            break
-        }
-    }
-}
-*/
