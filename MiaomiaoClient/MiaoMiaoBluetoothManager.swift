b1618359fc929d398e3817b65cd64f9ac55c2d34
dabear <bjorninge@gmail.com>
2019-04-23 12:42:47 +0200

    try to reconnect again  when connect fails

--- MiaomiaoClient/MiaoMiaoBluetoothManager.swift
+++ MiaomiaoClient/MiaoMiaoBluetoothManager.swift
@@ -372,6 +372,7 @@ final class MiaoMiaoBluetoothManager: NSObject, CBCentralManagerDelegate, CBPeri
             os_log("Did fail to connect peripheral error: %{public}@", log: MiaoMiaoBluetoothManager.bt_log, type: .error ,  "\(error.localizedDescription)")
         }
         state = .Disconnected
+        connect()
     }
     
     func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
