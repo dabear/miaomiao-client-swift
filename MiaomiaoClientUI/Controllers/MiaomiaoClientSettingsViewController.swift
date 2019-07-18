f042f5aab74032353bd9e1c9a1251c7234fe19db
dabear <bjorninge@gmail.com>
2019-07-18 23:57:05 +0200

    pumpmanager changes for jojo

--- MiaomiaoClientUI/Controllers/MiaomiaoClientSettingsViewController.swift
+++ MiaomiaoClientUI/Controllers/MiaomiaoClientSettingsViewController.swift
@@ -78,7 +78,21 @@ public class MiaomiaoClientSettingsViewController: UITableViewController, SubVie
 
         tableView.register(SettingsTableViewCell.self, forCellReuseIdentifier: SettingsTableViewCell.className)
         tableView.register(TextButtonTableViewCell.self, forCellReuseIdentifier: TextButtonTableViewCell.className)
+        
+        let button = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneTapped(_:)))
+        self.navigationItem.setRightBarButton(button, animated: false)
     }
+    
+    @objc func doneTapped(_ sender: Any) {
+        complete()
+    }
+    
+    private func complete() {
+        if let nav = navigationController as? SettingsNavigationViewController {
+            nav.notifyComplete()
+        }
+    }
+    
 
     // MARK: - UITableViewDataSource
 
@@ -519,7 +533,7 @@ public class MiaomiaoClientSettingsViewController: UITableViewController, SubVie
                     
                 }
                 
-                self.navigationController?.popViewController(animated: true)
+                self.complete()
             })
 
             present(confirmVC, animated: true) {
