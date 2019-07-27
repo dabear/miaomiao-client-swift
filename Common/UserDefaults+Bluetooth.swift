5829b76558cb6cf1ae0f547508657ca0995a9e62
dabear <bjorninge@gmail.com>
2019-07-27 14:11:18 +0200

    selected cell knows about it's own device

---
+++ Common/UserDefaults+Bluetooth.swift
@@ -0,0 +1,9 @@
+//
+//  UserDefaults+Bluetooth.swift
+//  MiaomiaoClientUI
+//
+//  Created by Bjørn Inge Berg on 27/07/2019.
+//  Copyright © 2019 Mark Wilson. All rights reserved.
+//
+
+import Foundation
---
+++ MiaomiaoClientUI/Cells/AnnotatedSubtitleCell.swift
@@ -0,0 +1,9 @@
+//
+//  AnnotatedSubtitleCell.swift
+//  MiaomiaoClientUI
+//
+//  Created by Bjørn Inge Berg on 27/07/2019.
+//  Copyright © 2019 Mark Wilson. All rights reserved.
+//
+
+import Foundation
